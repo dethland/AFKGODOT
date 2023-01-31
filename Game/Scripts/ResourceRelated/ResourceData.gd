d5e05627006630e04929531f98b3c26e61899a89
dethland <dethlanddeyouxiang@163.com>
2023-01-31 13:49:22 -0500

    [Update] resoucedata class

--- Game/Scripts/ResourceRelated/ResourceData.gd
+++ Game/Scripts/ResourceRelated/ResourceData.gd
@@ -1,5 +1,18 @@
-extends Resource
+extends Object
 class_name ResourceData
 
-var _name
-var amount
+var _name : set = set__name, get = get__name
+var amount : set = set_amount, get = get_amount
+
+
+func set__name(str_value):
+	_name = str_value
+	
+func get__name():
+	return _name
+	
+func set_amount(int_value):
+	amount = int_value
+	
+func get_amount():
+	return amount
