cbe3198523229c80f9b3f1e0e6938d756cf1148f
dethland <dethlanddeyouxiang@163.com>
2023-01-31 17:20:18 -0500

    [Update] add new functions to rdc class
    rdc for resource data container class

--- Game/Scripts/ResourceRelated/ResourceDataContainer.gd
+++ Game/Scripts/ResourceRelated/ResourceDataContainer.gd
@@ -4,4 +4,23 @@ class_name ResourceDataContainer
 var data_array = []
 
 func add_resource_data(data : ResourceData):
+	for res in data_array:
+		if res is ResourceData:
+			if res.get__name() != data.get__name(): # if same resource already there
+				continue
+			res.add_amount(data.get_amount())
+			return
+			
 	data_array.append(data)
+
+
+func remove_resource_data_by_name(str_value):
+	for res in data_array:
+		if res is ResourceData:
+			if res.get__name() != str_value:
+				continue
+			data_array.erase(res)
+
+
+
+
