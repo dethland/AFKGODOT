939d9e51528314b31fa4f563c045493bd55ce00e
dethland <dethlanddeyouxiang@163.com>
2023-01-31 17:35:53 -0500

    Update ResourceDataServer.gd

--- Game/Scripts/ResourceRelated/ResourceDataServer.gd
+++ Game/Scripts/ResourceRelated/ResourceDataServer.gd
@@ -1,5 +1,10 @@
-extends Object
+extends Node
 class_name ResourceDataServer
 
 func _get_world_data():
 	pass
+	
+func easy_resource_create(str_value : String, int_value : int):
+	var result = ResourceData.new()
+	result.set__name(str_value)
+	result.set_amount(int_value)
