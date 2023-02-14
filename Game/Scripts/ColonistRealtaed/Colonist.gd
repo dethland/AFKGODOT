f6d507bfab31c4a6ff8056cbdd10c087c5b31a59
henrim3 <112562805+henrim3@users.noreply.github.com>
2023-02-14 17:24:07 -0500

    added target_id

--- Game/Scripts/ColonistRealtaed/Colonist.gd
+++ Game/Scripts/ColonistRealtaed/Colonist.gd
@@ -1,9 +1,18 @@
 extends Node2D
 class_name Colonist
 
+var target_id_ : set = set_target_id, get = get_target_id
+
+func set_target_id(target_id):
+	target_id_ = target_id
+
+func get_target_id():
+	return target_id_
+
 func move_to(end_pos, delta):
 	var speed = 1000
 	var pos_diff = end_pos.x - position.x
+	# snap to end position if colonist would move past
 	if abs(pos_diff) < speed * delta:
 		position.x = end_pos.x
 		return
