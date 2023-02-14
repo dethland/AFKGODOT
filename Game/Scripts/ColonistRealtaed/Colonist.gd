b0777d350aa8571dd49c2aa916aa80696d71507f
henrim3 <112562805+henrim3@users.noreply.github.com>
2023-02-14 17:15:08 -0500

    added simple colonist x movement

--- Game/Scripts/ColonistRealtaed/Colonist.gd
+++ Game/Scripts/ColonistRealtaed/Colonist.gd
@@ -1,6 +1,17 @@
 extends Node2D
 class_name Colonist
 
+func move_to(end_pos, delta):
+	var speed = 1000
+	var pos_diff = end_pos.x - position.x
+	if abs(pos_diff) < speed * delta:
+		position.x = end_pos.x
+		return
+	if pos_diff > 0:
+		position.x += speed * delta
+	else:
+		position.x -= speed * delta
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +19,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	move_to(Vector2(1000, 0), delta)
