ca87c8a093f5ad69a4645323623b6f14f937d806
dethland <dethlanddeyouxiang@163.com>
2023-02-17 18:49:16 -0500

    Update ResourceDataServer.gd
    debug some function name change

--- Game/Scripts/ResourceRelated/ResourceDataServer.gd
+++ Game/Scripts/ResourceRelated/ResourceDataServer.gd
@@ -3,6 +3,6 @@ class_name ResourceDataServer
 	
 func easy_resource_create(str_value : String, int_value : int):
 	var result = ResourceData.new()
-	result.set__name(str_value)
+	result.set_name(str_value)
 	result.add_element("amount", int_value)
 	return result
