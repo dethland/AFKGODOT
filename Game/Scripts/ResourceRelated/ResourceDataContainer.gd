edbf0fdb55f8906335615d3569a7fd4daa11a62b
dethland <dethlanddeyouxiang@163.com>
2023-02-17 18:48:54 -0500

    Update ResourceDataContainer.gd
    update debug to the new resourceData structure and add temp function for testing

--- Game/Scripts/ResourceRelated/ResourceDataContainer.gd
+++ Game/Scripts/ResourceRelated/ResourceDataContainer.gd
@@ -11,6 +11,13 @@ func add_resource_data(data : ResourceData):
 		return -1
 	resources[resource_name] = data
 
+func stackable_add_resource_data(data : ResourceData):
+	var resource_name = data.get_name()
+	if resource_name in resources:
+		resources[resource_name].add_amount(data.get_amount())
+		return 
+	resources[resource_name] = data
+
 func remove_resource_data_by_name(resource_name):
 	if resource_name not in resources:
 		push_error("Resource already in container")
@@ -24,10 +31,10 @@ func getResourceDataContainer():
 
 ## help you debug what's inside the container, print all data
 func beautiful_debug():
-	print(data_array.size())
-	for data in data_array:
-		print(data)
-		if data is ResourceData:
-			print(data.get__name() + " : " + str(data.get_amount()))
+	print(resources.size())
+	for data_key in resources.keys():
+		print(resources[data_key])
+		if resources[data_key] is ResourceData:
+			print(resources[data_key].get_name() + " : " + str(resources[data_key].get_element("amount")))
 
 
