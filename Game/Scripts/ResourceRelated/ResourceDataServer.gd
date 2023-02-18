c9a129de76f3ceb8ce4739af52d77a18c57b56f0
dethland <dethlanddeyouxiang@163.com>
2023-02-18 13:24:42 -0500

    Update ResourceDataServer.gd

--- Game/Scripts/ResourceRelated/ResourceDataServer.gd
+++ Game/Scripts/ResourceRelated/ResourceDataServer.gd
@@ -1,8 +1,21 @@
 extends Node
 class_name ResourceDataServer
+
+var resource_ref_dic = {
+	"iron_ore" : {"type":"ore","tier":1,"amount":0,"icon_path":""},
+	"copper_ore" : {"type":"ore","tier":1,"amount":0,"icon_path":""}
+}
 	
 func easy_resource_create(str_value : String, int_value : int):
 	var result = ResourceData.new()
 	result.set__name(str_value)
 	result.add_element("amount", int_value)
 	return result
+
+func save_resource_ref_dic():
+	pass
+
+func load_resource_ref_dic():
+	pass
+	
+	
