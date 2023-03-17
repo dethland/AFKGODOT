7ec50050046ebedbe2dbecd71c6e2fa91a03cfae
dethland <dethlanddeyouxiang@163.com>
2023-03-17 18:40:57 -0400

    recipe_tool update

--- Game/Tools/recipe_tool.gd
+++ Game/Tools/recipe_tool.gd
@@ -1,11 +1,19 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+
+
+func _on_add_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_new_pressed():
+	pass # Replace with function body.
+
+
+func _on_save_pressed():
+	FS.
+
+
+func _on_load_pressed():
+	pass # Replace with function body.
