595ae30bf0a8b336b897384428405763403e89c9
diverh <123776591+diverh@users.noreply.github.com>
2023-03-17 18:20:37 -0400

    Cycle Tracker (Amount of days)

--- Game/Scripts/TimeRelated/time_server.gd
+++ Game/Scripts/TimeRelated/time_server.gd
@@ -1,13 +1,20 @@
 extends Node2D
 
 class_name TimeServer
-#add properties
+signal cycle_end
+
 var time = 0
+var gameday = 0
+var gamehour = 0
+var gameminute = 0
+var gamesecond = 0
+
 func _ready():
 	var timer = Timer.new()
 	add_child(timer)
 	timer.timeout.connect(_add_time)
-	timer.start(3)
+	timer.start(3) 
+	#15*, every 15 seconds one half hour passes so, 1 day is 360 seconds
 
 
 #function sets time 
@@ -15,11 +22,11 @@ func _add_time():
 	time += 0.5
 	if time == 24:
 		time = 0
+		emit_signal("cycle_end")
 	print(time)
 
 
 func _get_callert():
 	pass
-
 
 
