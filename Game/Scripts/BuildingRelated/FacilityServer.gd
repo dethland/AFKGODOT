c57fc153831b6d387fc32ef5e4950e76f5005db8
dethland <dethlanddeyouxiang@163.com>
2023-03-17 16:41:54 -0400

    Update FacilityServer.gd

--- Game/Scripts/BuildingRelated/FacilityServer.gd
+++ Game/Scripts/BuildingRelated/FacilityServer.gd
@@ -3,13 +3,26 @@ class_name FacilityServer
 
 var facility_array = []
 
-var facility_data_dic = {
+# the following data structure is no longer used, keep it only for debug purpose
+var archive_facility_data_dic = {
 	"OreRefineFactory" : {"type": Facility.facilityTypes.CONVERTE,
 		"time" : 10, "input" : ["iron_ore"], "output" : ["iron_bar"], "in_amount" : [3], 
 		"out_amount" : [1], "worker_capacity" : 4},
 	"MiningFactory" : {"type" : Facility.facilityTypes.GENERATE, 
 		"time" : 10, "output" : ["iron_ore"], "out_amount" : [5], "worker_capacity" : 4}
 }
+
+# this is te new data structure for the facility recipe data
+var facility_data_dic = {
+	"name" : {"type": Facility.facilityTypes.EMPTY,
+		"time" : 10, "input" : [], "output" : [], "worker_capacity" : 4}
+}
+
+
+# the structure need to change
+func add_facility_recipe(_name, type, time, input, output, worker):
+	pass
+
 
 func init_facility(facility_node : Facility):
 	facility_array.append(facility_node)
