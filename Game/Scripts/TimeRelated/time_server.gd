e7119e8dfc05a3e4c91d803fcfcb262e2cb9b350
diverh <123776591+diverh@users.noreply.github.com>
2023-03-31 16:26:50 -0400

    work/stop signals
    Sends a signal at time for workers to go to facility and a signal at time for workers to leave facility

--- Game/Scripts/TimeRelated/time_server.gd
+++ Game/Scripts/TimeRelated/time_server.gd
@@ -17,13 +17,18 @@ func _ready():
 	timer.start(3) 
 	#15*, every 15 seconds one half hour passes so, 1 day is 360 seconds
 
-
 #function sets time 
 func _add_time():
 	time += 0.5
 	if time == 24:
 		time = 0
 		emit_signal("cycle_end")
+	
+	if time == 6: #at 6am signal for workers to start.
+		emit_signal("begin_work")
+	if time == 18: #at 6pm signal for workers to go home.
+		emit_signal("stop_work")
+		
 	print(time)
 	
 	if time > 6 and time <= 18:
@@ -33,7 +38,6 @@ func _add_time():
 	elif time <= 6 and time > 18:
 		#something to set the time of night animation
 		$AnimationPlayer.play() #Just example
-
 
 func _get_callert():
 	pass
