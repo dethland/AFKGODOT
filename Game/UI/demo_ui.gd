5b772570f84712ae3853fd1aca804293d8a135c4
DiegoV03 <vazqud2@rpi.edu>
2023-04-04 16:47:35 -0400

    Demo Title Screen Updated

--- Game/UI/demo_ui.gd
+++ Game/UI/demo_ui.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_quit_button_pressed():
-	pass # Replace with function body.
+	get_tree().quit()
 
 func _on_play_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Tests/Testing/facility_play_ground.tscn")
