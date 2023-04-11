82ca6138296ce90c35d154a12ae8a821742becb1
dethland <dethlanddeyouxiang@163.com>
2023-04-11 18:13:11 -0400

    Update Facility.gd
    fix bug a lot

--- Game/Scripts/BuildingRelated/Facility.gd
+++ Game/Scripts/BuildingRelated/Facility.gd
@@ -41,6 +41,14 @@ var wait_manager_finish_bool := false
 
 func update_num_colonist_lable():
 	get_node("Num_colonist").text = str(get_population())
+	
+func update_inventory_lable():
+	var inventory = container.get_all_resource_data()
+	get_node("Inventory").text = ""
+	for item_name in inventory:
+		var item_amount = inventory[item_name].get_amount()
+		
+		get_node("Inventory").text += item_name + " "+str(item_amount) +"\n"
 
 func build_test_inventory():
 	for item_data in testing_inventory:
@@ -65,14 +73,19 @@ func add_timer_progress():
 func can_craft(recipe: Dictionary) -> bool:
 	return container.has_enough_resource(recipe["input"])
 
-func craft(recipe: Dictionary):
-	var temp;
+func craft():
+
 	if can_craft(recipe):
 		for item_index in range(0, recipe["input"].size()):
-			temp = recipe["input"][item_index].get_amount()
-			container.subtract_resource_element_quantity(recipe["input"][item_index], temp)
+			var item_amount = recipe["input"][item_index][1]
+			container.subtract_resource_element_quantity(recipe["input"][item_index][0], item_amount)
 		for item in recipe["output"]:
-			container.add_resource_data(item)
+			var item_name = item[0]
+			var item_amount = item[1]
+			var real_item = RDS.ref_resource_create(item_name, item_amount)
+			container.stackable_add_resource_data(real_item)
+			
+	update_inventory_lable()
 
 func send_request_for_colonist():
 	if facility_type != facilityTypes.HOUSING:
@@ -98,7 +111,7 @@ func generate_resource():
 	var item_list = RDS.generate_resource_by_recipe(recipe)
 	for item in item_list:
 		container.stackable_add_resource_data(item)
-	container.beautiful_debug()
+	update_inventory_lable()
 	emit_signal("item_changed")
 	
 func convert_resource():
@@ -140,6 +153,7 @@ func send_people_to():
 			instance.global_position = colonist_spawn_position
 			instance.set_workplace_id(request[0])
 			get_node(colonist_group_node_path).add_child(instance) # add colonist to the nodegroup
+			update_inventory_lable()
 			await get_tree().create_timer(3).timeout
 			continue # skip the rest of the loop
 
@@ -157,28 +171,31 @@ func send_people_to():
 	colonist_queue_list.clear()
 
 
-func send_resource_to(target_id, resource_data):
-	var resource_amount = resource_data.get_amount()
-	var resource_name = resource_data.get_name()
-	print("I am facility %s, I will send %s %s to facility %s" % [ID, resource_amount, resource_name, target_id])
-	var local_resource = container.get_resource_data_by_name(resource_data.get_name())
-	var curr_amount = local_resource.get_amount()
-	print(curr_amount)
-	#local_resource.set_amount(curr_amount - resource_amount)
-	var colonist = load(colonist_path)
-	var instance :Colonist = colonist.instantiate()
-	instance.global_position = colonist_spawn_position
-	instance.set_workplace_id(target_id)
-	instance.navi = get_parent().get_node("NaviServer")
-	get_parent().get_node("Colonists").add_child(instance)
+#func send_resource_to(target_id, resource_data):
+#	var resource_amount = resource_data.get_amount()
+#	var resource_name = resource_data.get_name()
+#	print("I am facility %s, I will send %s %s to facility %s" % [ID, resource_amount, resource_name, target_id])
+#	var local_resource = container.get_resource_data_by_name(resource_data.get_name())
+#	var curr_amount = local_resource.get_amount()
+#	print(curr_amount)
+#	#local_resource.set_amount(curr_amount - resource_amount)
+#	var colonist = load(colonist_path)
+#	var instance :Colonist = colonist.instantiate()
+#	instance.global_position = colonist_spawn_position
+#	instance.set_workplace_id(target_id)
+#	instance.navi = get_parent().get_node("NaviServer")
+#	get_parent().get_node("Colonists").add_child(instance)
 	
 func colonist_enter(colonist):
 	colonist.queue_free()
 	if colonist.get_resource_data() != null:
-		container.add_resource_data(colonist.get_resource_data())
+		container.stackable_add_resource_data(colonist.get_resource_data())
+		update_inventory_lable()
 		return # skip the rest of the code, for we don't want to add pepople when they have resource
+		
 	num_colonist += 1
 	update_num_colonist_lable()
+	
 	
 func colonist_exit():
 	num_colonist -= 1
@@ -216,6 +233,9 @@ func _ready():
 		facilityTypes.GENERATE:
 			timer.timeout.connect(generate_resource)
 
+	## debug
+	update_inventory_lable()
+
 func on_body_entered(body):
 	if body is CharacterBody2D:
 		# check the colonist target, if self delete body
@@ -242,5 +262,8 @@ func _on_requst_assign_finished():
 
 func _process(_delta):
 	if num_colonist >= recipe["worker_capacity"] and first_check:
+		if get__name() == 'ore_refine':
+			print("try to start timer")
+			print(recipe["time"])
 		timer.start(recipe["time"])
 		first_check = false	
